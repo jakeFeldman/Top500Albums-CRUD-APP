6447667dab3685eab1a830481397c7f5c43715d4
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-06-13 16:19:54 -0600

    Initialize KNEX and connect DB

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/top-500-album',
+  },
+  production: {
+    client: 'postgresql',
+    connection:
+  }
+
+};
